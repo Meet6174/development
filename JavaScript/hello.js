7280576ddc14a5b4a14d8551c494540ed86c10b6
Meet desai <jairamdesai18184@gmail.com>
2026-07-20 23:59:19 +0530

    hello in js

---
+++ JavaScript/hello.js
@@ -0,0 +1,6 @@
+console.log("Hello");
+let a = 9;
+let b = 2;
+let c = `val of a = ${a} and b = ${b} and the sum is ${a + b}`;
+console.log("the val of a=", a, "and b =", b, "and sum of both is :", a + b);
+console.log(c);
