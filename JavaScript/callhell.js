7b75ea18d4854e2eff71a49dc8b1c9bfe03fa537
Meet6174 <jairamdesai18184@gmail.com>
2026-08-04 00:59:18 +0530

    Add files via upload

---
+++ JavaScript/callhell.js
@@ -0,0 +1,70 @@
+// function db(x, pass, fail) {
+//   let netspeed = Math.floor(Math.random() * 10) + 1;
+//   let data = x;
+//   if (netspeed > 5) {
+//     pass();
+//   } else {
+//     fail();
+//   }
+// }
+//call back hall
+// db(
+//   "data 1",
+//   () => {
+//     console.log("data 1 is uploaded");
+//   },
+//   () => {
+//     console.log("Failed in data 1");
+//     db(
+//       "data 2",
+//       () => {
+//         console.log("data 2 is uploaded");
+//       },
+//       () => {
+//         console.log("Failed in data2");
+//         db(
+//           "data3",
+//           () => {
+//             console.log("data 3 is uploaded");
+//           },
+//           () => {
+//             console.log("Failed in data 3");
+//           },
+//         );
+//       },
+//     );
+//   },
+// );
+
+//promise chaining
+function db(x) {
+    return new Promise((resolve,reject)=>{
+        let netspeed = Math.floor(Math.random() * 10) + 1;
+        if (netspeed > 5) 
+        {
+            resolve("Success in "+x);//using + to combine string because then,catch only read first argument they ignore x to include
+        } 
+        else
+        {
+            reject("failed in "+x);
+        }
+    });
+}
+db("data 1")
+.then((val)=>
+{
+    console.log(val);
+    return  db("data 2");
+})
+.then((val)=>{
+    console.log(val);
+    return  db("data 3");
+})
+.then((val)=>{
+     console.log(val);
+})
+.catch((e)=>{
+    console.log(e);
+});
+
+// both code are same
